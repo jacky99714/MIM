555d66f2048041739b05e58764d75892ce3d1322
王宏翔 <wanghongxiang@MacBook-Pro.local>
2018-02-21 22:48:28 +0800

    test

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, request, abort
 
 from linebot import (
@@ -41,6 +42,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    sentGoogleSheet(event.message.text)
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
--- mvcTest.py
+++ mvcTest.py
@@ -1,6 +1,10 @@
-from flask import Flask, request
+import requests
 
 
-@app.route("/test", methods=['GET'])
-def test():
-    return 'OOL'
+def sentGoogleSheet(messageText):
+    r = requests.get(
+        "https://script.google.com/macros/s/AKfycbyjODcLO3-5jIqgkCy83fi9NZLz8kL_wRAT2CixKFA4/exec?data="+messageText+",rr,rrr&sheetUrl=https://docs.google.com/spreadsheets/d/1wOt1WAQ6puQAK63fHMpCS1D34wWvWqCqM_odNmqFW_U/edit&sheetTag=sheet1")
+
+
+if __name__ == "__main__":
+    sentGoogleSheet("eee")
