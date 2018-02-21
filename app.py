53385b9f3467c064d7ffc645cb19e4730c3a0df2
王宏翔 <wanghongxiang@MacBook-Pro.local>
2018-02-21 22:41:13 +0800

    test

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def handle_message(event):
 def sentGoogleSheet(messageText):
 
     r = requests.get(
-        "https://script.google.com/macros/s/AKfycbyjODcLO3-5jIqgkCy83fi9NZLz8kL_wRAT2CixKFA4/exec?data=rr,rr,rrr&sheetUrl=https://docs.google.com/spreadsheets/d/1wOt1WAQ6puQAK63fHMpCS1D34wWvWqCqM_odNmqFW_U/edit&sheetTag=sheet1")
+        "https://script.google.com/macros/s/AKfycbyjODcLO3-5jIqgkCy83fi9NZLz8kL_wRAT2CixKFA4/exec?data="+messageText+",rr,rrr&sheetUrl=https://docs.google.com/spreadsheets/d/1wOt1WAQ6puQAK63fHMpCS1D34wWvWqCqM_odNmqFW_U/edit&sheetTag=sheet1")
 
 
 import os
