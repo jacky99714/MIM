2a15ecaf284b7249903a4fc2e6f7d21dd7f13173
王宏翔 <wanghongxiang@MacBook-Pro.local>
2018-02-21 22:45:32 +0800

    test

--- app.py
+++ app.py
@@ -41,7 +41,6 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    sentGoogleSheet("測試")
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
