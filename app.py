8aba1ed5c05e63191d132494f4f0b0e310004e02
王宏翔 <wanghongxiang@MacBook-Pro.local>
2018-02-21 22:44:34 +0800

    test

--- app.py
+++ app.py
@@ -55,6 +55,5 @@ def sentGoogleSheet(messageText):
 
 import os
 if __name__ == "__main__":
-    sentGoogleSheet()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
